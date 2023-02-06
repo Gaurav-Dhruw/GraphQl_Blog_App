48499c5e23465c11f83c5b0a98a018e852ad3324
Gaurav Dhruw <dhruwgaurav01@gmail.com>
2023-02-06 20:31:05 +0530

    mobile ui updated

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -44,28 +44,28 @@ const PostDetail = ({post}) => {
       };
   return (
     <>
-        <div className='md:bg-white :bg-gradient-to-b from-transparent to-white shadow-lg rounded-lg p-8 mb-20'>
-            <div className=' overflow-hidden h-60 md:h-96 mb-14 rounded-md'>
+        <div className='md:bg-white bg-gradient-to-b from-transparent to-white shadow-lg rounded-lg p-8 mb-20'>
+            <div className=' overflow-hidden h-60 md:h-96 mb-8 md:mb-14 rounded-md'>
                 <img src={post.featuredImage.url} className='object-center w-full h-full'/>
             </div>
 
             <div className='lg:px-10 pb-14'>
             
-                <div className='lg:flex mb-5' >
-                    <div className='p-2 md:mr-4 flex items-center'>
+                <div className='lg:flex mb-3 text-sm md:text-base' >
+                    <div className='p-1 md:p-2 md:mr-4 flex items-center'>
                         <FcCalendar/>
                         <span className='ml-2'>
-                        Created at: {moment(post.createAt).format("MMM DD, YYYY")}
+                          Created at: {moment(post.createAt).format("MMM DD, YYYY")}
                         </span>
                     </div>
-                    {post.readTime && <div className="p-2 md:mr-4 flex items-center">
+                    {post.readTime && <div className="p-1 md:p-2 md:mr-4 flex items-center">
                         <FcClock/>
                         <span className='ml-2'>
                         {post.readTime} mins read 
                         </span> 
                     </div>}
                 </div>
-                <div className='font-bold text-xl md:text-3xl mb-10'>
+                <div className='font-bold text-xl md:text-3xl mb-6 md:mb-10'>
                     {post.title}
                 </div>
                 <div> 
--- pages/category/[slug].jsx
+++ pages/category/[slug].jsx
@@ -18,9 +18,9 @@ const CategoryPost = ({slug,posts, categories,tag}) => {
                 <title>Blog Hub</title>
                 <link rel='icon' href='/logo.png'/>
             </Head>
-            <div className='font-semibold text-center md:text-left md:text-lg mb-10 md:mx-10'>
+            <div className='font-semibold text-center md:text-left md:text-lg mt-10 mb-6 md:mt-0 md:mb-10 md:mx-10'>
                 Filtered Based on Tag :
-                <span className='trasition duration-200 px-3 py-1 ml-5 bg-slate-200 rounded-xl  hover:bg-slate-300 font-normal cursor-pointer text-base'>
+                <span className='trasition whitespace-nowrap duration-200 px-3 py-1 ml-5 bg-slate-200 rounded-xl  hover:bg-slate-300 font-normal cursor-pointer text-base'>
                     {tag.name}
                 </span>
             </div>
